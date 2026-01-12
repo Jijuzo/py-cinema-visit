242b1fb3c7cdf42100377416f99032591883d001
Igor Marczuk <usdreldo@gmail.com>
2026-01-12 22:04:17 +0100

    Revert "Solution #1"
    This reverts commit bf791c3f9f1c167d9af64ef84907b9f6fc1f42ab.

--- app/main.py
+++ app/main.py
@@ -1,20 +1,6 @@
-from app.people.cinema_staff import Cleaner
-from app.people.customer import Customer
-from app.cinema.bar import CinemaBar
-from app.cinema.hall import CinemaHall
+# write your imports here
 
 
-def cinema_visit(
-        customers: list,
-        hall_number: int,
-        cleaner: str,
-        movie: str
-) -> None:
-    customers_obj_list = []
-    for customer in customers:
-        customer_obj = Customer(customer["name"], customer["food"])
-        customers_obj_list.append(customer_obj)
-        CinemaBar.sell_product(customer_obj.food, customer_obj)
-    hall = CinemaHall(hall_number)
-    cleaner_obj = Cleaner(cleaner)
-    hall.movie_session(movie, customers_obj_list, cleaner_obj)
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+    # write you code here
+    pass
